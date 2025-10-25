32f573014ca2cfe75e7053a8d77d8fef72bcb00e
Mars <ruzicka.maty@gmail.com>
2025-10-26 00:33:22 +0200

    first

---
+++ common/src/main/java/com/mars/laserbridges/CommonClass.java
@@ -0,0 +1,68 @@
+package com.mars.laserbridges;
+
+import com.mars.deimos.config.DeimosConfig;
+import com.mars.laserbridges.platform.Services;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockSource;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.DyeItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+
+import static com.mars.laserbridges.Constants.*;
+import static com.mars.laserbridges.blocks.BridgeSourceBlock.COLOR;
+
+public class CommonClass {
+    public static void init() {
+        DeimosConfig.init(MOD_ID, LasersConfig.class);
+
+        DefaultDispenseItemBehavior dyeBehavior = new OptionalDispenseItemBehavior() {
+            protected ItemStack execute(BlockSource blockSource, ItemStack stack) {
+                ServerLevel serverLevel = blockSource.getLevel();
+                Direction direction = (Direction)blockSource.getBlockState().getValue(DispenserBlock.FACING);
+                BlockPos blockPos = blockSource.getPos().relative(direction);
+                BlockState blockState = serverLevel.getBlockState(blockPos);
+                Block block = blockState.getBlock();
+                if(block.equals(BuiltInRegistries.BLOCK.get(new ResourceLocation(MOD_ID, BRIDGE_SOURCE_BLOCK_NAME))) ||
+                        block.equals(BuiltInRegistries.BLOCK.get(new ResourceLocation(MOD_ID, FENCE_SOURCE_BLOCK_NAME)))){
+                    Item item = stack.getItem();
+                    int col = ((DyeItem) item).getDyeColor().getId();
+                    serverLevel.setBlock(blockPos, blockState.setValue(COLOR, col), 2);
+                    stack.shrink(1);
+                    serverLevel.gameEvent((Entity)null, GameEvent.BLOCK_CHANGE, blockPos);
+                    this.setSuccess(true);
+                    return stack;
+                }
+                return super.execute(blockSource, stack);
+            }
+        };
+
+        DispenserBlock.registerBehavior(Items.WHITE_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.ORANGE_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.MAGENTA_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.LIGHT_BLUE_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.YELLOW_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.LIME_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.PINK_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.GRAY_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.LIGHT_GRAY_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.CYAN_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.PURPLE_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.BLUE_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.BROWN_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.GREEN_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.RED_DYE, dyeBehavior);
+        DispenserBlock.registerBehavior(Items.BLACK_DYE, dyeBehavior);
+    }
+}
